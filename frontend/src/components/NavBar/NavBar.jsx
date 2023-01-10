dd8635050405ae0389f2874e298014686c034a00
Charles Taylor <charles4312@comcast.net>
2023-01-09 20:56:13 -0500

    Added Styles To NavBar Component

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,30 +1,71 @@
-import React from "react";
-import { useContext } from "react";
-import { useNavigate, Link } from "react-router-dom";
-import AuthContext from "../../context/AuthContext";
-import "./NavBar.css";
+import React, { useState, useEffect } from 'react';
+import useAuth from './../../hooks/useAuth';
+import Logo from '../../assets/Logo.png';
+import './NavBar.css';
+import axios from 'axios';
+// import ProfilePic from '../../assets/base_profile_picture.jpg'
 
-const Navbar = () => {
-  const { logoutUser, user } = useContext(AuthContext);
-  const navigate = useNavigate();
-  return (
-    <div className="navBar">
-      <ul>
-        <li className="brand">
-          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-            <b>React/Django JWT</b>
-          </Link>
-        </li>
-        <li>
-          {user ? (
-            <button onClick={logoutUser}>Logout</button>
-          ) : (
-            <button onClick={() => navigate("/login")}>Login</button>
-          )}
-        </li>
-      </ul>
-    </div>
-  );
-};
 
-export default Navbar;
+
+
+const NavBar = () => {
+
+    const [ProfilePic, setProfilePic] = useState("");
+    const [ProfileDec, setProfileDesc] = useState("");
+    const [user, token] = useAuth();
+
+    useEffect(() => {
+        getProfileInfo();
+    }, [])
+
+    const getProfileInfo = async (event) => {
+        // event.preventDefault();
+        console.log(user)
+        const response = await axios.get(`http://127.0.0.1:8000/api/profile/${user.id}/`)
+        let pictureData = `http://127.0.0.1:8000${response.data.profile_picture}`
+        setProfilePic(pictureData);
+        console.log(response.data.profile_picture)
+        //setProfilePic(response.data.profile_pic)
+
+        console.log(ProfilePic)
+        console.log(response);
+    }
+
+
+    return ( 
+        
+        <div className='navigation-bar'>
+            <div className='navigation-bar-logo'>
+                <img src = {Logo} alt='Gaming Corner Logo' className='navigation-bar-logo-image'/>
+            </div>
+
+            <div className='navigation-bar-title'>
+                <h1>Gaming Corner</h1>
+            </div>
+
+            <div className='navigation-bar-locations'>
+                <ul>
+                    <li>Home</li>
+                    <li>Profile</li>
+                    <li>Collection</li>
+                </ul>
+                <div>
+                    <img src={ProfilePic} className='navigation-bar-locations-user-profile-picture'/>
+                    <h2>Welcome, username</h2>
+                
+                </div>
+                {/* <div > */}
+
+                    
+
+                
+
+                {/* </div> */}
+                    
+            </div>  
+            
+        </div>
+     );
+}
+ 
+export default NavBar;
