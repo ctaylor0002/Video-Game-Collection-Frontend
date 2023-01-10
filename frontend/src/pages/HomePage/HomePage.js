5182002c9b0792738936af5acfa7446071b14ef2
ctaylor0002 <charles4312@comcast.net>
2023-01-10 07:25:33 -0500

    Added Method To Get All Posts From Backend Upon Viewing Home Screen + Began Styling Home Screen

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -9,8 +9,15 @@ const HomePage = (props) => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   //TODO: Add an AddCars Page to add a car for a logged in user's garage
-  // const [user, token] = useAuth();
+  const [user, token] = useAuth();
+  const [posts, setPosts] = useState([])
   // const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    fetchNewPosts();
+    console.log(posts);
+  }, [])
+
 
   // useEffect(() => {
   //   const fetchCars = async () => {
@@ -34,15 +41,39 @@ const HomePage = (props) => {
 
   // })
 
-  // const fetchNewPosts = async () => {
-  //   try {
-  //     let response = await axios.get(`http://127.0.0.1:8000/api/posts/${user.id}/`)
-  //   }
-  // }
+  const fetchNewPosts = async () => {
+     try {
+       let response = await axios.get(`http://127.0.0.1:8000/api/posts/${user.id}/`)
+       //console.log(response)
+       setPosts(response.data)
+     } catch (error) {
+      console.log(error.response.data);
+     }
+   }
 
   return (
     <div className="container">
-      <h1 className="test">Hello</h1>
+      {posts &&
+        posts.map((post) => (
+          <div className="container-post">
+            {console.log(post)}
+            <div className="container-post-user">
+              <h3 key={post.id}>{post.user.username}</h3>
+            </div>
+            <div className="container-post-content">
+              <p key={post.id}>{post.post_content}</p>
+            </div>
+            <div className="container-post-likes-dislikes">
+              <div className="container-post-likes-count">
+                <p key={post.id}>Likes: {post.likes}</p>
+              </div>
+              <div className="container-post-dislikes-count">
+                <p key={post.id}>Dislikes: {post.dislikes}</p>
+              </div>
+            </div>
+          </div>
+        ))
+      }
       {/* <h1>Home Page for {user.username}!</h1>
       {cars &&
         cars.map((car) => (
---
+++ frontend/src/pages/ProfilePage/ProfilePage.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+
+const ProfilePage = (props) => {
+    return ( 
+        <h2>Temporary Test!</h2>
+
+     );
+}
+ 
+export default ProfilePage;
