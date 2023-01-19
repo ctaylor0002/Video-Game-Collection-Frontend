d781d6c579f8b67dccabdb94547dc9a8a5ae1ef8
ctaylor0002 <charles4312@comcast.net>
2023-01-19 14:33:02 -0500

    Established A Method To Display Followed Users Posts On ProfilePage

--- frontend/src/pages/ProfilePage/ProfilePage.js
+++ frontend/src/pages/ProfilePage/ProfilePage.js
@@ -18,53 +18,91 @@ const ProfilePage = (props) => {
     // Plus I think I will have a following tab
    
     useEffect(() => {
+        //fetchFollowers();
         fetchNewPosts();
-        fetchFollowers();
-        getFollowersPosts();
-        console.log(posts);
-        console.log(followers);
-        console.log(followersPosts);
-
+        //getFollowersPosts();
       }, [])
 
     const fetchNewPosts = async () => {
         try {
-          let response = await axios.get(`http://127.0.0.1:8000/api/posts/${user.id}/`)
-          console.log(response.data);
-          setPosts(response.data);
-          console.log(posts);
+          let postsResponse = await axios.get(`http://127.0.0.1:8000/api/posts/${user.id}/`)
+          let tempPostsResponse = postsResponse.data;
+          let followersResponse = await axios.get(`http://127.0.0.1:8000/api/followers/`, {
+            headers: {
+                Authorization : "Bearer " + token,
+            },
+        });
+          let tempFollowersResponse = followersResponse.data;
+          let testPosts = [];
+
+          for(let i = 0; i < tempFollowersResponse.length; i++) {
+
+            let tempFollowersPosts = tempPostsResponse.filter(function(el) {
+
+                return el.user.id == tempFollowersResponse[i].follower_user
+            })   
+            testPosts = testPosts.concat(tempFollowersPosts)
+        
+        }
+        console.log(testPosts)
+        
+        setPosts(testPosts)
+       
         } catch (error) {
          console.log(error.response.data);
         }
       }
 
-    const fetchFollowers = async () => {
-        try{
-            let response = await axios.get(`http://127.0.0.1:8000/api/followers/`, {
-                headers: {
-                    Authorization : "Bearer " + token,
-                },
-            });
-            setFollowers(response.data);
-        } catch (error) {
-            console.log(error.response.data);
-        }
-    }
+    // const fetchFollowers = async () => {
+    //     try{
+    //         let response = await axios.get(`http://127.0.0.1:8000/api/followers/`, {
+    //             headers: {
+    //                 Authorization : "Bearer " + token,
+    //             },
+    //         });
+    //         console.log(response.data);
+    //         setFollowers(response.data);
+    //     } catch (error) {
+    //         console.log(error.response.data);
+    //     }
+    // }
 
-    const getFollowersPosts = async () => {
-        let tempFollowersPosts = posts.filter(post => post.user.id === user.id);
-        console.log(tempFollowersPosts);
-        setFollowersPosts(tempFollowersPosts);
-    }
+    // const getFollowersPosts = async () => {
+    //     let testPosts = [];
+    //     for(let i = 0; i < followers.length; i++) {
+    //         let tempFollowersPosts = posts.filter(post => post.user.id === followers[i]);
+    //         testPosts = [testPosts, tempFollowersPosts];
+    //         console.log(followersPosts)
+    //     }
+    //     console.log(testPosts);
+       
+        // try {
+        //     for(let i = 0; i < followers.length; i++) {
+        //         let tempFollowersPosts = posts.filter(post => post.user.id === followers[i]);
+        //         setFollowersPosts([...followersPosts, tempFollowersPosts]);
+        //         console.log(followersPosts)
+        //     } 
+        // } catch (error) {
+        //     console.log(error.response.data)
+        // }
+        
+       
+        
+        // let tempFollowersPosts = posts.filter(post => post.user.id === user.id);
+        // console.log(tempFollowersPosts);
+        // setFollowersPosts(tempFollowersPosts);
+    //}
+
+
    
     return ( 
         <div className='profile-container'>
             <div className='followers-posts'>
-                {followersPosts &&
-                followersPosts.map((post) => {
+                {posts &&
+                posts.map((post) => {
                     return (
                         <div>
-                            {console.log(post)};
+                            {console.log(post)}
                             <div className="container-post">
                                 {console.log(post)}
                                 <div className="container-post-user">
