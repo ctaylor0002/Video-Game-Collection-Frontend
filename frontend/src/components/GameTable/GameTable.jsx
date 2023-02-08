a6a449b98910c452c05f547aa44217923792b127
ctaylor0002 <charles4312@comcast.net>
2023-02-08 15:57:49 -0500

    Began Working On Table For Collection Page + Fixed Css Styles For Profile Page

---
+++ frontend/src/components/GameTable/GameTable.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import './GameTable.css'
+const GameTable = (props) => {
+
+    useEffect(() => {
+
+    }, [])
+
+
+    return ( 
+        <div>
+            <table>
+                <tbody>
+                    {props.collection.data &&
+                    props.collection.data.map((videoGame) => {
+                        {console.log(videoGame)}
+                        let completion
+                        {
+                            if (videoGame.completed == 0) {
+                                console.log(videoGame.completed == 0)
+                                completion = "Not Completed"
+                            } else {
+                                completion = "Completed"
+                        }}
+                        return (
+                            <tr>
+                                <td><img src={videoGame.video_game.video_game_image}/></td>
+                                {/* <td>{videoGame.user.username}</td> */}
+                                <td>{videoGame.video_game.video_game_title}</td>
+                                <td>{completion}</td>
+                                
+                            </tr>
+                        )
+                    })}
+                </tbody>
+                
+
+
+            </table>
+        </div>
+     );
+}
+ 
+export default GameTable;
--- frontend/src/pages/CollectionPage/CollectionPage.js
+++ frontend/src/pages/CollectionPage/CollectionPage.js
@@ -1,8 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import GameTable from '../../components/GameTable/GameTable';
+import useAuth from '../../hooks/useAuth';
+import axios from 'axios';
 
 const CollectionPage = (props) => {
+    const [user, token] = useAuth()
+    const [collection, setCollection] = useState([]);
+ 
+    useEffect(() => {
+        getCollection();
+    }, [])
+    async function getCollection() {
+        try {
+            let tempCollection = await axios.get(`http://127.0.0.1:8000/api/collection/${user.id}`, {
+                headers: {
+                    Authorization : "Bearer " + token,
+                },
+            });
+            setCollection(tempCollection);
+        } catch (error) {
+            console.log(error.response.data);
+        }
+    }
+
     return ( 
-        <h2>Collection Page</h2>
+        <div>
+        <GameTable collection={collection} getCollection={getCollection}/>
+
+
+        </div>
      );
 }
  
